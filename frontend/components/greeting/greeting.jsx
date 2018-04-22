c7e48903631f09ca034c81e63633de4fe7b143d4
zach kramer <zkramer22@gmail.com>
2018-04-22 14:14:57 -0400

    begin refactoring css to fix weird double scrollbar problem on small window size. Splash page fixed, need to work on other components.

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Greeting = ({currentUser, logout}) => {
+const Greeting = ({ currentUser, logout }) => {
 
   return (
     <div className="splash-flexbox">
@@ -9,28 +9,29 @@ const Greeting = ({currentUser, logout}) => {
       <div className="BLACKround">
         <div className="greeting-background">
         </div>
-      </div>
-
-      <div id="greeting-cont" className="greeting-cont">
-        <div className="main-logo">
-          <img id="spotiFLY-logo"
-            src="https://www.clipartqueen.com/image-files/bird-flight-silhouette.png"/>
-          <h2>SpotiFLY</h2>
-        </div>
-        <Link className="signup-link" to='/signup'>Signup</Link>
-        <div id="account-question">-- ALREADY HAVE AN ACCOUNT? --</div>
+        <div id="greeting-cont" className="greeting-cont">
+          <div className="main-logo">
+            <img id="spotiFLY-logo"
+              src="https://www.clipartqueen.com/image-files/bird-flight-silhouette.png"/>
+            <h2>SpotiFLY</h2>
+          </div>
+          <Link className="signup-link" to='/signup'>Signup</Link>
+          <div id="account-question">-- ALREADY HAVE AN ACCOUNT? --</div>
           <Link id="login-link-button" className="login-link" to='/login'>Login</Link>
+        </div>
+
+        <div id="welcome-message" className="welcome-message">
+          <h1>Music takes flight.</h1>
+          <h3>Listen to zillions of songs for free.</h3>
+          <br></br>
+          <ul className="welcome-bullets">
+            <li><strong>&#10003;</strong> Search & discover music anytime, anywhere</li>
+            <li><strong>&#10003;</strong> Create playlists of your favorites</li>
+          </ul>
+        </div>
       </div>
 
-      <div id="welcome-message" className="welcome-message">
-        <h1>Music takes flight.</h1>
-        <h3>Listen to zillions of songs for free.</h3>
-        <br></br>
-        <ul className="welcome-bullets">
-          <li><strong>&#10003;</strong> Search & discover music anytime, anywhere</li>
-          <li><strong>&#10003;</strong> Create playlists of your favorites</li>
-        </ul>
-      </div>
+
     </div>
   );
 };
